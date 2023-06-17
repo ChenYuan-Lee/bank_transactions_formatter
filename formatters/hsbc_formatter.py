0ad1996d65fd3f925b1c57eecf6197bf9d66beaf
ChenYuan-Lee <leechenyuan.95@gmail.com>
2023-06-17 12:08:06 +0800

    Add HSBCFormatter

---
+++ formatters/hsbc_formatter.py
@@ -0,0 +1,33 @@
+from enum import Enum
+from typing import List
+
+from data_models import Header
+from formatters.csv_formatter import CSVFormatter
+
+
+class HSBCColumns(Enum):
+    DATE = Header(col_num=0)
+    DESCRIPTION = Header(col_num=1)
+    DEPOSIT_OR_WITHDRAWAL = Header(col_num=2)
+
+
+class HSBCFormatter(CSVFormatter):
+    __bank_specific_columns__ = HSBCColumns
+    __bank_name__ = "HSBC"
+
+    FILE_NAME = "hsbc.csv"
+    HAS_HEADER_ROW = False
+    INPUT_DATE_FORMAT = "%d/%m/%Y"
+
+    @classmethod
+    def custom_format_row(cls, row: List[str], output_row: list) -> None:
+        output_row[cls.__consolidated_columns__.TRANSACTION.value.col_num] = \
+            row[cls.__bank_specific_columns__.DESCRIPTION.value.col_num]
+
+        deposit_or_withdrawal = row[cls.__bank_specific_columns__.DEPOSIT_OR_WITHDRAWAL.value.col_num]
+        if cls.is_positive_value(deposit_or_withdrawal):
+            output_row[cls.__consolidated_columns__.DEPOSIT.value.col_num] = \
+                cls.get_abs_value(deposit_or_withdrawal)
+        else:
+            output_row[cls.__consolidated_columns__.WITHDRAWAL.value.col_num] = \
+                cls.get_abs_value(deposit_or_withdrawal)
