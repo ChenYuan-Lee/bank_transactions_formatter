037cecd738cb3e006fa2edf730c953704cb0151e
ChenYuan-Lee <leechenyuan.95@gmail.com>
2023-06-24 19:52:14 +0800

    Create Selector class to filter by existence of input files

---
+++ selector.py
@@ -0,0 +1,19 @@
+import os
+from typing import List
+
+from file_dt_checker import FileDTChecker
+from formatters.bank_formatter import BankFormatter
+
+
+class Selector:
+    @classmethod
+    def transform(cls, formatters: List[BankFormatter]) -> List[BankFormatter]:
+        existing_files = []
+        selected_formatters = []
+        for formatter in formatters:
+            fp = formatter.get_input_file_path()
+            if os.path.isfile(fp):
+                existing_files.append(fp)
+                selected_formatters.append(formatter)
+        FileDTChecker.transform(existing_files)
+        return selected_formatters
